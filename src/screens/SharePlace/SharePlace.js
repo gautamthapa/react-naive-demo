d62fbb93a67448ac27c47f178546f28f170e1c7a
im-gautam <gautam.thapa22@gmail.com>
2019-03-23 22:50:02 +0530

    11. Updating Redux

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from "react-redux";
+
+import PlaceInput from "../../components/PlaceInput/PlaceInput";
+import { addPlace } from "../../store/actions/index";
 
 class SharePlaceScreen extends Component{
+
+    placeAddedHandler = placeName => {
+        this.props.onAddPlace(placeName);
+    }
+
     render(){
         return(
             <View>
-                <Text>On SharePlaceScreen</Text>
+                <PlaceInput onPlaceAdded={this.placeAddedHandler} />
             </View>
         );
     }
 }
 
-export default SharePlaceScreen;
+const mapDispathToProps = dispath => {
+    return {
+        onAddPlace: (placeName) => dispath(addPlace(placeName))
+    };
+};
+
+export default connect(null, mapDispathToProps)(SharePlaceScreen);
