fdfa2c84643d832b8478f80d8060e5e11a274b45
im-gautam <gautam.thapa22@gmail.com>
2019-03-25 00:49:23 +0530

    16. Styling the Side Drawer

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
 
 class SideDrawer extends Component {
     render (){
         return(
-            <View style={[styles.container, {width: Dimensions.get("window").width * 0.8 }]}>
-                <Text>SideDrawer</Text>
+            <View style={[styles.container,
+             {width: Dimensions.get("window").width * 0.8 }]}>
+                <TouchableOpacity>
+                    <View style={styles.drawerItem}>
+                        <Icon 
+                        name="ios-log-out"
+                        size={30}
+                        color="#aaa"
+                        style={styles.drawerItemIcon} />
+                        <Text>SideDrawer</Text>
+                    </View>    
+                </TouchableOpacity>
             </View>
 
         );
@@ -14,9 +25,18 @@ class SideDrawer extends Component {
 
 const styles = StyleSheet.create({
      container: {
-         paddingTop: 22,
+         paddingTop: 50,
          backgroundColor: "white",
          flex: 1
+     },
+     drawerItem : {
+         flexDirection: "row",
+         alignItems: "center",
+         padding: 10,
+         backgroundColor: "#eee"
+     },
+     drawerItemIcon: {
+         marginRight: 10
      }
 });
 
