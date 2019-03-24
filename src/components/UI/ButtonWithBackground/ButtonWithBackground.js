7114114cab2554ba478bfb4c980a8033ba69b4d5
im-gautam <gautam.thapa22@gmail.com>
2019-03-25 01:14:45 +0530

    20. Using the Platform API

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, View, StyleSheet, TouchableNativeFeedback, Platform } from 'react-native';
 
-const buttonWithbackground = props => (
-    <TouchableOpacity onPress={props.onPress}>
+const buttonWithbackground = props => {
+
+    const content = (
         <View style={[styles.button, {backgroundColor: props.color}]}>
             <Text>{props.children}</Text>
         </View>
-    </TouchableOpacity>
-);
+    );
+    if(Platform.OS === 'android'){
+           return (
+               <TouchableNativeFeedback onPress={props.onPress} >
+                {content}
+               </TouchableNativeFeedback>
+           );
+    }
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+        {content}
+        </TouchableOpacity>
+    );
+    
+};
 
 const styles = StyleSheet.create({
     button: {
